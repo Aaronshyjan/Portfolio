cf279482c81bf95d8eb5f562e1123ec5c3bb0d0b
Aaron <aaronshyjan2019@gmail.com>
2026-02-27 15:16:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <BackgroundDots />
